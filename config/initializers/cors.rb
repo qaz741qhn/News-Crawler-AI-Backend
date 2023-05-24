d6cd4026aa1be919e5b690f02129f39449460545
Ching Lee <qaz741qhn@gmail.com>
2023-05-25 02:05:11 +0900

    add cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3001', 'localhost:3000'
+    origins 'localhost:3001', 'localhost:3000', 'https://news-crawler-ai.herokuapp.com/'
 
     resource '*',
       headers: :any,
