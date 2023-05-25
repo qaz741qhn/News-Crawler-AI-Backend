13fa9c5bd65ce3981d6aa4488856fc9535d61e30
Ching Lee <qaz741qhn@gmail.com>
2023-05-25 16:46:08 +0900

    add new domain

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module AiNews
     config.api_only = true
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'localhost:3001', 'localhost:3000', 'news-crawler-ai.herokuapp.com'
+        origins 'localhost:3001', 'localhost:3000', 'news-crawler-ai.herokuapp.com', 'news-crawler-ai-frontend.vercel.app'
         resource '*',
           :headers => :any,
           :methods => [:get, :post, :delete, :put, :patch, :options, :head],
