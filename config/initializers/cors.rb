135136b96cf4f5447e66071af6d30385e0401e3c
Ching Lee <qaz741qhn@gmail.com>
2023-06-08 12:28:50 +0900

    remove unused cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3001', 'localhost:3000', 'news-crawler-ai.herokuapp.com', 'news-crawler-ai.vercel.app', 'job-generator-application.herokuapp.com', 'job-intro-questions-generator.vercel.app', 'code-assistant-frontend.vercel.app'
+    origins 'localhost:3001', 'localhost:3000', 'news-crawler-ai.vercel.app', 'job-intro-questions-generator.vercel.app', 'code-assistant-frontend.vercel.app'
 
     resource '*',
       headers: :any,
