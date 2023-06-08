56a161a9ef89410c1be2699048d98ff9bdd4c454
Ching Lee <qaz741qhn@gmail.com>
2023-06-08 20:42:41 +0900

    add OmniAuth session

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,10 @@ module AiNews
     config.load_defaults 7.0
     config.openai_client = OpenAI::Client.new(api_key: ENV["OPENAI_API_KEY"])
     config.api_only = true
+    config.session_store :cookie_store, key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies # Required for all session management
+    config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
+
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins 'localhost:3001', 'localhost:3000', 'news-crawler-ai.vercel.app', 'job-intro-questions-generator.vercel.app', 'code-assistant-frontend.vercel.app'
