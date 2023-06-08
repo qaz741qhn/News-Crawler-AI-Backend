938835c43de69ee7d0a07086bdd6de530e6f1243
Ching Lee <qaz741qhn@gmail.com>
2023-06-08 12:01:43 +0900

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
+
+  get '/', to: 'home#index'
+  
+  # Job application
+  resources :job_applications do
+    collection do
+      post :generate_interview_question
+      post :generate_content
+    end
+  end  
+  resources :generated_histories, only: [:index, :create, :show]
+
+  # AI News
   resources :news, only: [:index, :show]
   resources :visits, only: [:create]
+
+  # Code Assistant
+  post '/generate', to: 'requests#create'
+  get '/capabilities', to: 'capabilities#index'
+  post '/detect_available_languages', to: 'code_conversion#detect_available_languages'
+  post '/convert_code', to: 'code_conversion#convert_code'
 end
